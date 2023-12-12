cf07d22d09f4a5850d6935a1df142cc0ffec5aa6
adarshnagrikar14 <adarshnagrikar1404@gmail.com>
2023-12-12 23:14:15 +0530

    Update 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,8 @@ dependencies {
     implementation("com.google.android.gms:play-services-auth:20.7.0")
     implementation("com.airbnb.android:lottie:6.2.0")
 
+    implementation("net.dongliu:apk-parser:2.4.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/sih/rakshak/SecurityScanActivity.java
+++ app/src/main/java/com/sih/rakshak/SecurityScanActivity.java
@@ -4,12 +4,30 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.annotation.SuppressLint;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.text.TextUtils;
 import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import net.dongliu.apk.parser.ApkParser;
+import net.dongliu.apk.parser.bean.ApkMeta;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SecurityScanActivity extends AppCompatActivity {
+    private TextView fileTextView, resultTextview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +42,64 @@ public class SecurityScanActivity extends AppCompatActivity {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
 
+        fileTextView = findViewById(R.id.scanFeedLive);
+        resultTextview = findViewById(R.id.scanResult);
+
+        new ApkScanTask().execute();
+
     }
 
+    @SuppressLint("StaticFieldLeak")
+    private class ApkScanTask extends AsyncTask<Void, String, Void> {
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            List<String> apkFilePaths = getAllInstalledApks();
+
+            for (String apkFilePath : apkFilePaths) {
+                publishProgress(apkFilePath);
+                try {
+                    Thread.sleep(300);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return null;
+        }
+
+        private List<String> getAllInstalledApks() {
+            List<String> apkFilePaths = new ArrayList<>();
+
+            List<PackageInfo> packages = getPackageManager().getInstalledPackages(0);
+            for (PackageInfo packageInfo : packages) {
+                ApplicationInfo appInfo = packageInfo.applicationInfo;
+                String apkFilePath = appInfo.sourceDir;
+                apkFilePaths.add(apkFilePath);
+            }
+
+            return apkFilePaths;
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+            for (String value : values) {
+                fileTextView.setSingleLine();
+                fileTextView.setEllipsize(TextUtils.TruncateAt.START);
+                fileTextView.setText(value);
+            }
+        }
+
+        @SuppressLint("SetTextI18n")
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            fileTextView.setText("Scanning Done.");
+            resultTextview.setText("No Threat Found!");
+            Toast.makeText(SecurityScanActivity.this, "Scanning Completed", Toast.LENGTH_SHORT).show();
+        }
+    }
 
     private void setNavColor() {
         Window window = getWindow();
